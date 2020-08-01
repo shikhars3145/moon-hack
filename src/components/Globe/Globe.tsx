875a9e61e94e80a8318075050484e52d553e5214
Donghyeon Kim <0916dhkim@gmail.com>
2020-08-01 10:14:47 +0900

    Add Globe Container

---
+++ src/components/Globe/Globe.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./Globe.module.scss";
+
+function Globe() {
+  return (
+    <div className={style.container}>Globe</div>
+  );
+}
+
+export default Globe;
