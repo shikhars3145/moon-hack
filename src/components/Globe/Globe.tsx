54c4262a01d1e37404740a4e251df48e02a0b5bb
Donghyeon Kim <0916dhkim@gmail.com>
2020-08-02 14:47:58 +0900

    Add 3D Marker On The Globe

--- src/components/Globe/Globe.tsx
+++ src/components/Globe/Globe.tsx
@@ -1,6 +1,6 @@
-import React, { useMemo, useEffect } from "react";
+import React, { useMemo, useEffect, useState } from "react";
 import style from "./Globe.module.scss";
-import { TextureLoader, MeshStandardMaterial, SphereBufferGeometry, PerspectiveCamera, Vector3 } from "three";
+import { TextureLoader, MeshStandardMaterial, SphereBufferGeometry, Vector3, ConeBufferGeometry } from "three";
 import { Canvas, useThree, useFrame } from "react-three-fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import earthImage from "./earthmap1k.jpg";
@@ -33,6 +33,18 @@ function useSphere(radius: number) {
 }
 
 /**
+ * Make a cone geometry.
+ * @param radius Cone radius
+ * @param height Cone height
+ * @param radialSegment Number of segments
+ */
+function useCone(radius: number, height: number, radialSegment: number = 12) {
+  return useMemo(() => {
+    return new ConeBufferGeometry(radius, height, radialSegment);
+  }, [radius, height, radialSegment]);
+}
+
+/**
  * Use orbit control.
  * @param target Focused coordinates.
  */
@@ -54,14 +66,56 @@ function useCameraControl(target: Vector3) {
   useFrame(() => controls.update());
 };
 
-type Props = {
+type MarkerProps = {
+  longitude: number,
+  latitude: number
+}
+
+function degToRad(deg: number) {
+  return deg * Math.PI / 180;
+}
+
+function Marker({longitude, latitude}: MarkerProps) {
+  const geometry = useCone(0.1, 0.5);
+
+  // Convert coordinates to cartesian.
+  const position = useMemo<[number, number, number]>(() => {
+    return [
+      EARTH_RADIUS * Math.cos(degToRad(latitude)) * Math.cos(degToRad(longitude)),
+      EARTH_RADIUS * Math.sin(degToRad(latitude)),
+      -EARTH_RADIUS * Math.cos(degToRad(latitude)) * Math.sin(degToRad(longitude))
+    ];
+  }, [longitude, latitude]);
+
+  const rotation = useMemo<[number, number, number]>(() => {
+    return [
+      0,
+      longitude * Math.PI / 180,
+      (latitude + 90) * Math.PI / 180
+    ];
+  }, [longitude, latitude]);
+
+  const [hovered, setHover] = useState(false);
+  return (
+    <mesh
+      position={position}
+      rotation={rotation}
+      geometry={geometry}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}>
+      <meshStandardMaterial attach="material" color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  )
+}
+
+type EarthProps = {
   position: [number, number, number]
 };
 
 /**
  * 3D Earth Object.
  */
-function Earth(props: Props) {
+function Earth(props: EarthProps) {
   const earthMaterial = useMaterial(earthImage);
   const earthShape = useSphere(EARTH_RADIUS);
 
@@ -81,7 +135,7 @@ function Globe() {
     <div className={style.container}>
       <Canvas>
         <ambientLight />
-        <Earth position={[-20, 0, 0]} />
+        <Earth position={[0, 0, 0]} />
       </Canvas>
     </div>
   );
