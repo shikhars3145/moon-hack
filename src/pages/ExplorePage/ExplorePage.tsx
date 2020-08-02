cae6ba058a0077d93897da71dd4560ee67ce77d0
Donghyeon Kim <0916dhkim@gmail.com>
2020-08-02 17:01:01 +0900

    Add Typescript to ExplorePage

---
+++ src/pages/ExplorePage/ExplorePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Globe from "../../components/Globe/Globe";
+
+function ExplorePage() {
+  return (
+    <div className="explore-page">
+      <Globe />
+    </div>
+  );
+}
+
+export default ExplorePage;
