7ff1445548408f981980222d03bde8f18a357ee5
Donghyeon Kim <0916dhkim@gmail.com>
2020-08-02 23:11:46 +0900

    Replace Dummy Images

--- src/pages/ExplorePage/ExplorePage.tsx
+++ src/pages/ExplorePage/ExplorePage.tsx
@@ -3,37 +3,37 @@ import Globe, { ImageData } from "../../components/Globe/Globe";
 
 const dummy_image_data: ImageData[] = [
   {
-    url: "http://discovertajima.com/upimg/a91936126b087216c4caec1bf90c778a/720x720.jpg",
+    url: "https://in-the-sky.org/images/constellations/con_TAU_001.png",
     timestamp: 0,
     latitude: 37,
     longitude: 127
   },
   {
-    url: "http://discovertajima.com/upimg/a91936126b087216c4caec1bf90c778a/720x720.jpg",
+    url: "https://in-the-sky.org/images/constellations/con_TAU_001.png",
     timestamp: 0,
     latitude: 40,
     longitude: -74
   },
   {
-    url: "http://discovertajima.com/upimg/a91936126b087216c4caec1bf90c778a/720x720.jpg",
+    url: "https://in-the-sky.org/images/constellations/con_TAU_001.png",
     timestamp: 0,
     latitude: 43,
     longitude: -79
   },
   {
-    url: "http://discovertajima.com/upimg/a91936126b087216c4caec1bf90c778a/720x720.jpg",
+    url: "https://in-the-sky.org/images/constellations/con_TAU_001.png",
     timestamp: 0,
     latitude: 51,
     longitude: 0
   },
   {
-    url: "http://discovertajima.com/upimg/a91936126b087216c4caec1bf90c778a/720x720.jpg",
+    url: "https://in-the-sky.org/images/constellations/con_TAU_001.png",
     timestamp: 0,
     latitude: 30,
     longitude: 31
   },
   {
-    url: "http://discovertajima.com/upimg/a91936126b087216c4caec1bf90c778a/720x720.jpg",
+    url: "https://in-the-sky.org/images/constellations/con_TAU_001.png",
     timestamp: 0,
     latitude: -34,
     longitude: 151
