62036838a6abb23005e2c86d3c55ac65cbb4cfcb
Donghyeon Kim <0916dhkim@gmail.com>
2020-08-02 17:18:51 +0900

    Add Dummy Image Data

--- src/pages/ExplorePage/ExplorePage.tsx
+++ src/pages/ExplorePage/ExplorePage.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
-import Globe from "../../components/Globe/Globe";
+import React, { useState } from 'react';
+import Globe, { ImageData } from "../../components/Globe/Globe";
+
+const dummy_image_data: ImageData[] = [
+  {
+    url: "",
+    timestamp: 0,
+    latitude: 37,
+    longitude: 127
+  },
+  {
+    url: "",
+    timestamp: 0,
+    latitude: 40,
+    longitude: -74
+  },
+  {
+    url: "",
+    timestamp: 0,
+    latitude: 43,
+    longitude: -79
+  },
+  {
+    url: "",
+    timestamp: 0,
+    latitude: 51,
+    longitude: 0
+  },
+  {
+    url: "",
+    timestamp: 0,
+    latitude: 30,
+    longitude: 31
+  },
+  {
+    url: "",
+    timestamp: 0,
+    latitude: -34,
+    longitude: 151
+  },
+];
 
 function ExplorePage() {
+  const [images, setImages] = useState<ImageData[]>(dummy_image_data);
   return (
     <div className="explore-page">
-      <Globe />
+      <Globe images={images} />
     </div>
   );
 }
